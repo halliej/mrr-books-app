cf4bf39bec16058482770966f6361c2919356362
Hal DeLage <hal.delage@teamaol.com>
2017-01-15 15:04:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Books Application
+
+The Books application from the Modern React and Redux course on Udemy.com.
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'JS: The Good Parts', pages: 349 },
+    { title: 'Harry Potter', pages: 421 },
+    { title: 'The Dark Tower', pages: 863 },
+    { title: 'Eloquent Ruby', pages: 12 }
+  ];
+}
